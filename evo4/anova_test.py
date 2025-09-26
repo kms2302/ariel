4d89f1b815bddb632c141e206121ffaf729b9d15
kms2302 <kms2302@github.com>
2025-09-26 19:05:09 +0200

    Fix bugs in ANOVA test

--- evo4/anova_test.py
+++ evo4/anova_test.py
@@ -1,6 +1,8 @@
 ## ANOVA STATISTICAL TEST ##
 import scipy.stats as stats
 import wandb
+from pathlib import Path
+import pandas as pd
 
 # Same directory
 from params import ENTITY, PROJECT, ALGOS, SEEDS
@@ -11,8 +13,10 @@ def main():
     run = wandb.init(
         entity=ENTITY,
         project=PROJECT,
+        name="Analysis",
         job_type="analysis",
         reinit=True,
+        config={"Experiment": "Analysis"},
     )
 
     algo_best_per_run = {algo: [] for algo in ALGOS}  # collect 1 value per run
@@ -36,9 +40,6 @@ def main():
                 best_overall = max(all_f)
                 algo_best_per_run[algo].append(best_overall)
     
-    # Close analysis run
-    run.finish()
-
     # Final fitness per seed
     baseline_runs = algo_best_per_run["Baseline"]
     ga_runs       = algo_best_per_run["GA"]
@@ -47,17 +48,26 @@ def main():
     # Run One-Way ANOVA
     f_statistic, p_value = stats.f_oneway(baseline_runs, ga_runs, cmaes_runs)
 
+    # Log results to Weights & Biases
+    run.log({
+        "F-statistic": f_statistic,
+        "P-value": p_value,
+    })
+
     # Output results
     print("One-Way ANOVA on Final Fitness Scores")
     print("-------------------------------------")
-    print(f"F-statistic: {f_statistic:.4f}")
-    print(f"P-value:     {p_value:.4f}")
+    print(f"F-statistic: {f_statistic:.5f}")
+    print(f"P-value:     {p_value:.5f}")
 
     # Interpretation
     if p_value < 0.05:
         print("Significant difference found between at least two groups.")
     else:
         print("No significant difference found.")
+    
+    # Close analysis run
+    run.finish()
 
 
 if __name__ == "__main__":
